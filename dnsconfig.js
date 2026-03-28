82b513f259b7174191477edec5d26e20374d39d4
Nick Lopez <20211302+nlopez@users.noreply.github.com>
2026-03-28 15:32:48 -0400

    ci test

--- dnsconfig.js
+++ dnsconfig.js
@@ -1,57 +1,78 @@
 // Providers
 
-var REG_NONE = NewRegistrar('none');
-var DNS_CLOUDFLARE = NewDnsProvider('cloudflare');
+var REG_NONE = NewRegistrar("none");
+var DNS_CLOUDFLARE = NewDnsProvider("cloudflare");
 var DNS_BIND = NewDnsProvider("bind", {
-  "default_soa": {
-    "master": "ns1.radoncanyon.com.",
-    "mbox": "dnsadmin.radoncanyon.com.",
-    "refresh": 900,
-    "retry": 900,
-    "expire": 1800,
-    "minttl": 60,
+  default_soa: {
+    master: "ns1.radoncanyon.com.",
+    mbox: "dnsadmin.radoncanyon.com.",
+    refresh: 900,
+    retry: 900,
+    expire: 1800,
+    minttl: 60,
   },
-  "default_ns": [
-    "ns1.radoncanyon.com.",
-  ]
-})
+  default_ns: ["ns1.radoncanyon.com."],
+});
 
 // Domains
 
-D('desertbluffs.com',
+D(
+  "desertbluffs.com",
   REG_NONE,
   DnsProvider(DNS_CLOUDFLARE),
   DefaultTTL(1), // Auto
   NO_PURGE,
   CF_PROXY_DEFAULT_OFF,
-  IGNORE_NAME('lga1', 'A'),
-  IGNORE_NAME('lga2', 'A'),
+  IGNORE_NAME("lga1", "A"),
+  IGNORE_NAME("lga2", "A"),
   IGNORE("_acme-challenge", "TXT"),
   IGNORE("_acme-challenge.**", "TXT"),
   // ALIAS('@', 'lga1.desertbluffs.com.'),
-  A('unifi', '68.183.136.131', CF_PROXY_OFF),
-  AAAA('unifi', '2604:a880:400:d1::81a:6001', CF_PROXY_OFF),
-  CAA('@', 'iodef', 'mailto:sslabuse@desertbluffs.com', CAA_CRITICAL),
-  CAA('@', 'issue', 'letsencrypt.org'),
-  CAA('@', 'issuewild', 'letsencrypt.org'),
-  CNAME('ec25ukpzz6h4nddab3jyevcnzmmmbrgm._domainkey', 'ec25ukpzz6h4nddab3jyevcnzmmmbrgm.dkim.amazonses.com.', CF_PROXY_OFF),
-  CNAME('gc4caluqqezmfk6xanwpdpw5eytglrkn._domainkey', 'gc4caluqqezmfk6xanwpdpw5eytglrkn.dkim.amazonses.com.', CF_PROXY_OFF),
-  CNAME('ptusb247sybmwk4i32j5lmagtmtqq7qi._domainkey', 'ptusb247sybmwk4i32j5lmagtmtqq7qi.dkim.amazonses.com.', CF_PROXY_OFF),
-  CNAME('zb14086362', 'zmverify.zoho.com.', CF_PROXY_OFF),
-  MX('@', 10, 'mx.zoho.com.'),
-  MX('@', 20, 'mx2.zoho.com.'),
-  MX('@', 50, 'mx3.zoho.com.'),
-  TXT('@', '"v=spf1 mx include:zoho.com include:ses.desertbluffs.com ~all"'),
-  TXT('*', '"v=spf1 -all"'),
-  TXT('@', '"google-site-verification=NNYObey8Yje_tBPGc8bIsGlU5ii5KAePBJvDVGO2S9M"'),
-  MX('ses', 10, 'feedback-smtp.us-east-1.amazonses.com.'),
-  TXT('_amazonses', '"nOSqNz6GuHWUV/GdUWiXEWRTTRMK/ibv6afYRtn5WtU="'),
-  TXT('_dmarc', '"v=DMARC1; p=reject; pct=100; rua=mailto:itadso9z@ag.us.dmarcian.com"'),
-  TXT('ses', '"v=spf1 include:amazonses.com ~all"'),
-  TXT('zoho._domainkey', '"v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCIqHMhuUXI0KCxNVfkJpHSKVKqFFGi5zB2xmnvsz6tkKNSUHpDLUg2PXy3xkSep9V0XKMjD8zWSRcjFUCj/JB0d3WGdJ7RblrKDpMqkj4M8dWJPCkCrLFLw2ET6D2eCx0W/zrW1foAP/HcXv10QYiE5iOh+dUNHTyLFTHJzzVnoQIDAQAB"'),
+  A("unifi", "68.183.136.131", CF_PROXY_OFF),
+  AAAA("unifi", "2604:a880:400:d1::81a:6001", CF_PROXY_OFF),
+  CAA("@", "iodef", "mailto:sslabuse@desertbluffs.com", CAA_CRITICAL),
+  CAA("@", "issue", "letsencrypt.org"),
+  CAA("@", "issuewild", "letsencrypt.org"),
+  CNAME(
+    "ec25ukpzz6h4nddab3jyevcnzmmmbrgm._domainkey",
+    "ec25ukpzz6h4nddab3jyevcnzmmmbrgm.dkim.amazonses.com.",
+    CF_PROXY_OFF,
+  ),
+  CNAME(
+    "gc4caluqqezmfk6xanwpdpw5eytglrkn._domainkey",
+    "gc4caluqqezmfk6xanwpdpw5eytglrkn.dkim.amazonses.com.",
+    CF_PROXY_OFF,
+  ),
+  CNAME(
+    "ptusb247sybmwk4i32j5lmagtmtqq7qi._domainkey",
+    "ptusb247sybmwk4i32j5lmagtmtqq7qi.dkim.amazonses.com.",
+    CF_PROXY_OFF,
+  ),
+  CNAME("zb14086362", "zmverify.zoho.com.", CF_PROXY_OFF),
+  MX("@", 10, "mx.zoho.com."),
+  MX("@", 20, "mx2.zoho.com."),
+  MX("@", 50, "mx3.zoho.com."),
+  TXT("@", '"v=spf1 mx include:zoho.com include:ses.desertbluffs.com ~all"'),
+  TXT("*", '"v=spf1 -all"'),
+  TXT(
+    "@",
+    '"google-site-verification=NNYObey8Yje_tBPGc8bIsGlU5ii5KAePBJvDVGO2S9M"',
+  ),
+  MX("ses", 10, "feedback-smtp.us-east-1.amazonses.com."),
+  TXT("_amazonses", '"nOSqNz6GuHWUV/GdUWiXEWRTTRMK/ibv6afYRtn5WtU="'),
+  TXT(
+    "_dmarc",
+    '"v=DMARC1; p=reject; pct=100; rua=mailto:itadso9z@ag.us.dmarcian.com"',
+  ),
+  TXT("ses", '"v=spf1 include:amazonses.com ~all"'),
+  TXT(
+    "zoho._domainkey",
+    '"v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCIqHMhuUXI0KCxNVfkJpHSKVKqFFGi5zB2xmnvsz6tkKNSUHpDLUg2PXy3xkSep9V0XKMjD8zWSRcjFUCj/JB0d3WGdJ7RblrKDpMqkj4M8dWJPCkCrLFLw2ET6D2eCx0W/zrW1foAP/HcXv10QYiE5iOh+dUNHTyLFTHJzzVnoQIDAQAB"',
+  ),
 );
 
-D('radoncanyon.com',
+D(
+  "radoncanyon.com",
   REG_NONE,
   DnsProvider(DNS_CLOUDFLARE),
   DefaultTTL(1), // Auto
@@ -59,56 +80,101 @@ D('radoncanyon.com',
   CF_PROXY_DEFAULT_OFF,
   IGNORE("_acme-challenge", "TXT"),
   IGNORE("_acme-challenge.**", "TXT"),
-  A('gw.lga1', '192.168.222.1'),
-  A('gw.lga2', '192.168.239.254'),
-  A('knode1.lga1', '192.168.222.143'),
-  A('knode2.lga1', '192.168.222.144'),
-  A('nas1.lga1', '192.168.222.149'),
-  A('nas2.lga1', '192.168.222.114'),
-  A('odroid1.lga1', '100.127.60.59'),
-  A('odroid2.lga1', '100.110.65.2'),
-  TXT('@', '"google-site-verification=ZnkGcjiK9_Od1OOq6ZxMFzoS3Q7ZatXb8W_aH89g5mY"'),
+  A("gw.lga1", "192.168.222.1"),
+  A("gw.lga2", "192.168.239.254"),
+  A("knode1.lga1", "192.168.222.143"),
+  A("knode2.lga1", "192.168.222.144"),
+  A("nas1.lga1", "192.168.222.149"),
+  A("nas2.lga1", "192.168.222.114"),
+  A("odroid1.lga1", "100.127.60.59"),
+  A("odroid2.lga1", "100.110.65.2"),
+  TXT(
+    "@",
+    '"google-site-verification=ZnkGcjiK9_Od1OOq6ZxMFzoS3Q7ZatXb8W_aH89g5mY"',
+  ),
   // https://www.gov.uk/guidance/protect-domains-that-dont-send-email
-  TXT('*', '"v=spf1 -all"'),
-  TXT('*._domainkey', '"v=DKIM1; p="'),
-  TXT('_dmarc', '"v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; fo=1; rua=mailto:itadso9z@ag.us.dmarcian.com"'),
-  MX('@', 0, '.'),
-  NS('sslip', 'ns-aws.sslip.io.'),
-  NS('sslip', 'ns-gce.sslip.io.'),
-  NS('sslip', 'ns-azure.sslip.io.'),
+  TXT("*", '"v=spf1 -all"'),
+  TXT("*._domainkey", '"v=DKIM1; p="'),
+  TXT(
+    "_dmarc",
+    '"v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; fo=1; rua=mailto:itadso9z@ag.us.dmarcian.com"',
+  ),
+  MX("@", 0, "."),
+  NS("sslip", "ns-aws.sslip.io."),
+  NS("sslip", "ns-gce.sslip.io."),
+  NS("sslip", "ns-azure.sslip.io."),
 );
 
-D('aethertrail.com',
+D(
+  "aethertrail.com",
   REG_NONE,
   DnsProvider(DNS_CLOUDFLARE),
   DefaultTTL(1), // Auto
   CF_PROXY_DEFAULT_OFF,
   // Keep this for continued ProtonMail verification
-  TXT('@', '"protonmail-verification=f1ab807d903a45b94591fa70864d795a030e118c"'),
-  MX('@', 10, 'mail.protonmail.ch.'),
-  MX('@', 20, 'mailsec.protonmail.ch.'),
-  TXT('@', '"v=spf1 include:_spf.protonmail.ch mx ~all"'),
-  TXT('*', '"v=spf1 -all"'),
-  TXT('@', '"google-site-verification=dvq0zeVnXreX-ORHQopNOs_c7T3fqdl5LAATDoL6miU"'),
-  CNAME('protonmail._domainkey', 'protonmail._domainkey.d37mwtwdoeonibgpu5jnsfnzrjnqm5p4ifgp2plkf5r4wkgmkrlha.domains.proton.ch.'),
-  CNAME('protonmail2._domainkey', 'protonmail2._domainkey.d37mwtwdoeonibgpu5jnsfnzrjnqm5p4ifgp2plkf5r4wkgmkrlha.domains.proton.ch.'),
-  CNAME('protonmail3._domainkey', 'protonmail3._domainkey.d37mwtwdoeonibgpu5jnsfnzrjnqm5p4ifgp2plkf5r4wkgmkrlha.domains.proton.ch.'),
-  TXT('_dmarc', '"v=DMARC1; p=quarantine; rua=mailto:itadso9z@ag.us.dmarcian.com"')
-)
+  TXT(
+    "@",
+    '"protonmail-verification=f1ab807d903a45b94591fa70864d795a030e118c"',
+  ),
+  MX("@", 10, "mail.protonmail.ch."),
+  MX("@", 20, "mailsec.protonmail.ch."),
+  TXT("@", '"v=spf1 include:_spf.protonmail.ch mx ~all"'),
+  TXT("*", '"v=spf1 -all"'),
+  TXT(
+    "@",
+    '"google-site-verification=dvq0zeVnXreX-ORHQopNOs_c7T3fqdl5LAATDoL6miU"',
+  ),
+  CNAME(
+    "protonmail._domainkey",
+    "protonmail._domainkey.d37mwtwdoeonibgpu5jnsfnzrjnqm5p4ifgp2plkf5r4wkgmkrlha.domains.proton.ch.",
+  ),
+  CNAME(
+    "protonmail2._domainkey",
+    "protonmail2._domainkey.d37mwtwdoeonibgpu5jnsfnzrjnqm5p4ifgp2plkf5r4wkgmkrlha.domains.proton.ch.",
+  ),
+  CNAME(
+    "protonmail3._domainkey",
+    "protonmail3._domainkey.d37mwtwdoeonibgpu5jnsfnzrjnqm5p4ifgp2plkf5r4wkgmkrlha.domains.proton.ch.",
+  ),
+  TXT(
+    "_dmarc",
+    '"v=DMARC1; p=quarantine; rua=mailto:itadso9z@ag.us.dmarcian.com"',
+  ),
+);
 
-D('nulogorsk.com',
+D(
+  "nulogorsk.com",
   REG_NONE,
   DnsProvider(DNS_CLOUDFLARE),
   CF_PROXY_DEFAULT_OFF,
   // Keep this for continued ProtonMail verification
-  TXT('@', '"protonmail-verification=95474329be7c5db7b13b98a1a7cf2a302dea3f57"'),
-  MX('@', 10, 'mail.protonmail.ch.'),
-  MX('@', 20, 'mailsec.protonmail.ch.'),
-  TXT('@', '"v=spf1 include:_spf.protonmail.ch mx ~all"'),
-  TXT('*', '"v=spf1 -all"'),
-  CNAME('protonmail._domainkey', 'protonmail.domainkey.dtq25ze44gqbgofgt2yteu67cedbavwyaszrouvla23bp3c2ltjuq.domains.proton.ch.'),
-  CNAME('protonmail2._domainkey', 'protonmail2.domainkey.dtq25ze44gqbgofgt2yteu67cedbavwyaszrouvla23bp3c2ltjuq.domains.proton.ch.'),
-  CNAME('protonmail3._domainkey', 'protonmail3.domainkey.dtq25ze44gqbgofgt2yteu67cedbavwyaszrouvla23bp3c2ltjuq.domains.proton.ch.'),
-  TXT('_dmarc', '"v=DMARC1; p=quarantine; rua=mailto:itadso9z@ag.us.dmarcian.com"'),
-  TXT('@', '"google-site-verification=5812hCoCmKwaI_0BOxwDLgllxUt0Z1zF8xMVGpnJSJ4"')
-)
+  TXT(
+    "@",
+    '"protonmail-verification=95474329be7c5db7b13b98a1a7cf2a302dea3f57"',
+  ),
+  MX("@", 10, "mail.protonmail.ch."),
+  MX("@", 20, "mailsec.protonmail.ch."),
+  TXT("@", '"v=spf1 include:_spf.protonmail.ch mx ~all"'),
+  TXT("*", '"v=spf1 -all"'),
+  CNAME(
+    "protonmail._domainkey",
+    "protonmail.domainkey.dtq25ze44gqbgofgt2yteu67cedbavwyaszrouvla23bp3c2ltjuq.domains.proton.ch.",
+  ),
+  CNAME(
+    "protonmail2._domainkey",
+    "protonmail2.domainkey.dtq25ze44gqbgofgt2yteu67cedbavwyaszrouvla23bp3c2ltjuq.domains.proton.ch.",
+  ),
+  CNAME(
+    "protonmail3._domainkey",
+    "protonmail3.domainkey.dtq25ze44gqbgofgt2yteu67cedbavwyaszrouvla23bp3c2ltjuq.domains.proton.ch.",
+  ),
+  TXT(
+    "_dmarc",
+    '"v=DMARC1; p=quarantine; rua=mailto:itadso9z@ag.us.dmarcian.com"',
+  ),
+  TXT(
+    "@",
+    '"google-site-verification=5812hCoCmKwaI_0BOxwDLgllxUt0Z1zF8xMVGpnJSJ4"',
+  ),
+  TXT("testing", "it works!"),
+);
